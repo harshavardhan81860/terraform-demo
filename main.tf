3504e60543aa0807403307bd3c2bdfa9fad3b169
harshavardhan81860 <harsha81860@gmail.com>
2026-05-25 18:27:53 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -17,7 +17,7 @@ module "s3" {
 module "ec2" {
   source          = "./modules/ec2"
   ami             = "ami-0f58b397bc5c1f2e8"
-  instance_type   = "t2.micro"
+  instance_type   = "t2.medium"
   subnet_id       = module.vpc.subnet_id
   sg_id           = module.sg.sg_id
 }
