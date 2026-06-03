38966c36c6ebcae2a2cfc07358fb4c9dfd0828d9
harshavardhan81860 <harsha81860@gmail.com>
2026-06-03 15:24:10 +0530

    Refactor outputs in outputs.tf for clarity

--- modules/vpc/outputs.tf
+++ modules/vpc/outputs.tf
@@ -1,7 +1,2 @@
-output "vpc_id" {
-  value = aws_vpc.main.id
-}
-
-output "subnet_id" {
-  value = aws_subnet.public.id
-}
+output "vpc_id" { value = aws_vpc.main.id }
+output "public_subnet_id" { value = aws_subnet.public.id }
