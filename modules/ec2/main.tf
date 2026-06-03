c54a6897975f1cbd8863c6afde249d9ac57e2412
harshavardhan81860 <harsha81860@gmail.com>
2026-06-03 15:28:19 +0530

    Update main.tf

--- modules/ec2/main.tf
+++ modules/ec2/main.tf
@@ -1,13 +1,8 @@
-resource "aws_instance" "ec2" {
-  ami                         = var.ami
-  instance_type               = var.instance_type
-  subnet_id                   = var.subnet_id
-  vpc_security_group_ids      = [var.sg_id]
-  associate_public_ip_address = true
+resource "aws_instance" "web" {
+  ami                    = var.ami_id
+  instance_type          = var.instance_type
+  subnet_id              = var.subnet_id
+  vpc_security_group_ids = [var.security_group_id]
 
-  key_name = "terraform-key"
-
-  tags = {
-    Name = "demo-ec2"
-  }
+  tags = { Name = "yakkai-app-server" }
 }
